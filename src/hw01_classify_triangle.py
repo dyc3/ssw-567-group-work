4475f0a2ac0fb4e9c3ea7f150116dc96395c77d5
EltonVaz <90809989+EltonVaz@users.noreply.github.com>
2024-01-25 18:39:56 -0500

    classify triangle function (#2)
    * classify triangle function
    
    * lint fixes

---
+++ src/hw01_classify_triangle.py
@@ -0,0 +1,15 @@
+"Module to creat classify_traingle function."
+
+def classify_triangle(a,b,c):
+    "Function classifying traingle into different types based on each side's length."
+    if a + b <= c or b + c <= a or a + c <= b:
+        return "Invalid Triangle"
+    if a == b == c:
+        return "Equilateral"
+    if a == b or b == c or a == c:
+        if round(a*a) + round(b*b) == round(c*c):
+            return "Right Isosceles Triangle"
+        return "Isosceles Triangle"
+    if round(a*a) + round(b*b) == round(c*c):
+        return "Right Scalene Triangle"
+    return "Scalene"

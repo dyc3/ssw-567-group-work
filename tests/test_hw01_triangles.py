86a4dee56d2cee4a1942ffe1d900b5864cc766ed
Carson McManus <carson.mcmanus1@gmail.com>
2024-01-27 12:28:21 -0500

    hw01: refactor and fix `classify_triangle` to pass all tests

--- tests/test_hw01_triangles.py
+++ tests/test_hw01_triangles.py
@@ -1,4 +1,4 @@
-from src.hw01_classify_triangle import classify_triangle
+from src.hw01_classify_triangle import classify_triangle, is_triangle, is_right_triangle
 
 from hypothesis import given, strategies as st, assume
 
@@ -7,7 +7,7 @@ from hypothesis import given, strategies as st, assume
     a=st.integers(min_value=1), b=st.integers(min_value=1), c=st.integers(min_value=1)
 )
 def test_valid_triangles_ints(a: int, b: int, c: int):
-    assume(a + b > c and a + c > b and b + c > a)
+    assume(is_triangle(a, b, c))
     assert classify_triangle(a, b, c) != "NotATriangle"
 
 
@@ -17,7 +17,7 @@ def test_valid_triangles_ints(a: int, b: int, c: int):
     c=st.floats(min_value=0.1, max_value=100000000),
 )
 def test_valid_triangles_floats(a: float, b: float, c: float):
-    assume(a + b > c and a + c > b and b + c > a)
+    assume(is_triangle(a, b, c))
     assert classify_triangle(a, b, c) != "NotATriangle"
 
 
@@ -25,7 +25,7 @@ def test_valid_triangles_floats(a: float, b: float, c: float):
     a=st.integers(min_value=1), b=st.integers(min_value=1), c=st.integers(min_value=1)
 )
 def test_invalid_triangles(a: int, b: int, c: int):
-    assume(not (a + b > c and a + c > b and b + c > a))
+    assume(not is_triangle(a, b, c))
     assert classify_triangle(a, b, c) == "NotATriangle"
 
 
@@ -35,9 +35,9 @@ def test_equilateral_triangle(num: int):
 
 
 def test_right_triangle_simple():
-    assert "Right" in classify_triangle(3, 4, 5)
-    assert "Right" in classify_triangle(1, 1, 2**0.5)
-    assert "Right" in classify_triangle(2**0.5, 1, 1)
+    assert is_right_triangle(3, 4, 5)
+    assert is_right_triangle(1, 1, 2**0.5)
+    assert is_right_triangle(2**0.5, 1, 1)
 
 
 @given(a=st.integers(min_value=1), b=st.integers(min_value=1))
@@ -52,10 +52,27 @@ def test_isosceles_triangle(a: int, b: int):
 @given(a=st.integers(min_value=1), b=st.integers(min_value=1))
 def test_right_triangle(a: int, b: int):
     c = (a**2 + b**2) ** 0.5
-    assume(a + b > c and a + c > b and b + c > a)
-    assert "Right" in classify_triangle(a, b, c)
-    assert "Right" in classify_triangle(b, a, c)
-    assert "Right" in classify_triangle(b, c, a)
-    assert "Right" in classify_triangle(c, b, a)
-    assert "Right" in classify_triangle(c, a, b)
-    assert "Right" in classify_triangle(a, c, b)
+    assume(is_triangle(a, b, c))
+    assert is_right_triangle(a, b, c)
+    assert is_right_triangle(b, a, c)
+    assert is_right_triangle(b, c, a)
+    assert is_right_triangle(c, b, a)
+    assert is_right_triangle(c, a, b)
+    assert is_right_triangle(a, c, b)
+
+
+@given(
+    a=st.floats(min_value=0.1, max_value=100000000),
+    b=st.floats(min_value=0.1, max_value=100000000),
+    c=st.floats(min_value=0.1, max_value=100000000),
+)
+def test_classify_output_format(a: float, b: float, c: float):
+    assert classify_triangle(a, b, c) in (
+        "Equilateral",
+        "Isosceles",
+        "Scalene",
+        "Equilateral Right",
+        "Isosceles Right",
+        "Scalene Right",
+        "NotATriangle",
+    )

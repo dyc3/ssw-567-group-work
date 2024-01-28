34cf2d41713dda95ad2278f9dc48f9bde3321a4f
Carson McManus <dyc3@users.noreply.github.com>
2024-01-27 19:07:55 -0500

    hw01: refactor and fix `classify_triangle` to pass all tests (#4)

--- src/hw01_classify_triangle.py
+++ src/hw01_classify_triangle.py
@@ -1,16 +1,40 @@
-"Module to creat classify_traingle function."
+import math
+from typing import Literal
 
 
-def classify_triangle(a, b, c):
-    "Function classifying traingle into different types based on each side's length."
-    if a + b <= c or b + c <= a or a + c <= b:
-        return "Invalid Triangle"
+def classify_triangle(a: float, b: float, c: float) -> str:
+    tri_type: str = triangle_class(a, b, c)
+    if tri_type == "NotATriangle":
+        return tri_type
+
+    if is_right_triangle(a, b, c):
+        tri_type += " Right"
+    return tri_type.strip()
+
+
+def is_triangle(a: float, b: float, c: float) -> bool:
+    """Returns True if a, b, and c can form a triangle, False otherwise."""
+    return a + b > c and a + c > b and b + c > a
+
+
+def triangle_class(
+    a: float, b: float, c: float
+) -> Literal["Equilateral", "Isosceles", "Scalene", "NotATriangle"]:
+    """Returns the type of triangle, or "NotATriangle" if a, b, and c cannot form a triangle."""
+    if not is_triangle(a, b, c):
+        return "NotATriangle"
     if a == b == c:
         return "Equilateral"
-    if a == b or b == c or a == c:
-        if round(a * a) + round(b * b) == round(c * c):
-            return "Right Isosceles Triangle"
-        return "Isosceles Triangle"
-    if round(a * a) + round(b * b) == round(c * c):
-        return "Right Scalene Triangle"
-    return "Scalene"
+    elif len(set([a, b, c])) == 2:
+        return "Isosceles"
+    else:
+        return "Scalene"
+
+
+def is_right_triangle(a: float, b: float, c: float) -> bool:
+    """Returns True if a, b, and c form a right triangle, False otherwise."""
+    return (
+        math.isclose(a**2 + b**2, c**2, rel_tol=1e-3)
+        or math.isclose(b**2 + c**2, a**2, rel_tol=1e-3)
+        or math.isclose(a**2 + c**2, b**2, rel_tol=1e-3)
+    )
